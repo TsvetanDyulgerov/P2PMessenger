dc289388106296699beeff86564a67f17d322513
Tsvetan Dyulgerov <tsvetan.dyulgerov@gmail.com>
2025-01-30 15:15:48 +0200

    Initial Commit

---
+++ P2PMessenger/P2PMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  P2PMessengerApp.swift
+//  P2PMessenger
+//
+//  Created by Tsvetan Dyulgerov on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct P2PMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
